e151b11df46e0fe27f563304daa54c2f3e509fd3
leeduckgo <albertschr@163.com>
2021-10-01 21:00:36 +0800

    feat/change contracts in app.jsx and 00_deploy_your_contract.js

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -244,10 +244,10 @@ function App(props) {
   ]);
 
   // keep track of a variable from the contract in the local React state:
-  const purpose = useContractReader(readContracts, "YourContract", "purpose");
+  const purpose = useContractReader(readContracts, "PurposeHandler", "purpose");
 
   // 📟 Listen for broadcast events
-  const setPurposeEvents = useEventListener(readContracts, "YourContract", "SetPurpose", localProvider, 1);
+  const setPurposeEvents = useEventListener(readContracts, "PurposeHandler", "SetPurpose", localProvider, 1);
 
   /*
   const addressFromENS = useResolveName(mainnetProvider, "austingriffith.eth");
@@ -453,7 +453,7 @@ function App(props) {
               }}
               to="/"
             >
-              YourContract
+              PurposeHandler
             </Link>
           </Menu.Item>
           <Menu.Item key="/hints">
@@ -507,7 +507,7 @@ function App(props) {
             */}
 
             <Contract
-              name="YourContract"
+              name="PurposeHandler"
               signer={userSigner}
               provider={localProvider}
               address={address}
